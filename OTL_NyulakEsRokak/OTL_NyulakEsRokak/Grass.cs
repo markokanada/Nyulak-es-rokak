60c7b5d637e8424a742ac3fc7c19bef167aa33f7
csokamarton <csoka.marton2005@gmail.com>
2023-10-09 22:04:01 +0200

    GetTheTypeOfGrass() Implement

--- OTL_NyulakEsRokak/OTL_NyulakEsRokak/Grass.cs
+++ OTL_NyulakEsRokak/OTL_NyulakEsRokak/Grass.cs
@@ -8,8 +8,29 @@ namespace OTL_NyulakEsRokak
 {
 	internal class Grass
 	{
-		public Grass()
+		public Grass(int nutritionalValue)
 		{
+			NutritionalValue = nutritionalValue;
 		}
-	}
+
+		public int NutritionalValue { get; init; }
+
+		public string GetTheTypeOfGrass()
+		{
+			if (this.NutritionalValue == 0) 
+			{
+				return "Fű kezdemény";
+			}
+			else if (this.NutritionalValue == 1)
+			{
+				return "Zsenge fű";
+			}
+			else if (this.NutritionalValue == 2)
+			{
+				return "Fű csomó";
+			}
+			return "";
+		}
+
+    }
 }
