de62b1f9b947503924d8740f9bf79c38fb2f42c4
csokamarton <csoka.marton2005@gmail.com>
2023-10-11 18:14:00 +0200

    EntitiesLivingTogether init

---
+++ OTL_NyulakEsRokak/OTL_NyulakEsRokak/EntitiesLivingTogether.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTL_NyulakEsRokak
+{
+	internal class EntitiesLivingTogether
+	{
+	}
+}
