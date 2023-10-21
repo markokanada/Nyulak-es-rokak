3dd6a001596f7a8a115779043db83306f31dce09
molmardo <molnarmark2005@gmail.com>
2023-10-21 10:02:05 +0700

    Map Save

--- OTL_NyulakEsRokak/OTL_NyulakEsRokak/Map.cs
+++ OTL_NyulakEsRokak/OTL_NyulakEsRokak/Map.cs
@@ -4,23 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace OTL_NyulakEsRokak
 {
     internal class Map
     {
-            
         private readonly int width;
         private readonly int height;
         private readonly Random random = new Random();
         private readonly Grass[,] grassMatrix;
+        private readonly List<Rabbits> rabbitsList = new List<Rabbits>();
+        private readonly List<Foxes> foxesList = new List<Foxes>();
+        public int GetWidth()
+        {
+            return width;
+        }
 
+        public int GetHeight()
+        {
+            return height;
+        }
 
-        //Map settings
         public Map(int width, int height)
         {
             this.width = width;
             this.height = height;
+            grassMatrix = new Grass[width, height];
 
+            InitializeMap();
         }
 
         private void InitializeMap()
@@ -29,11 +40,67 @@ namespace OTL_NyulakEsRokak
             {
                 for (int j = 0; j < height; j++)
                 {
-                    grassMatrix[i, j] = new Grass(0);
+                     grassMatrix[i, j] = new Grass(0);
                 }
             }
         }
 
+        public void PlaceGrass(int x, int y, int nutritionalValue)
+        {
+            if (x >= 0 && x < width && y >= 0 && y < height)
+            {
+                grassMatrix[x, y] = new Grass(nutritionalValue);
+            }
+        }
 
+        public void PlaceRabbit(int x, int y, int satietyMeter)
+        {
+            if (x >= 0 && x < width && y >= 0 && y < height)
+            {
+                Rabbits rabbit = new Rabbits(satietyMeter);
+                rabbitsList.Add(rabbit);
+            }
+        }
+
+        public void PlaceFox(int x, int y, int satietyMeter)
+        {
+            if (x >= 0 && x < width && y >= 0 && y < height)
+            {
+                Foxes fox = new Foxes(satietyMeter);
+                foxesList.Add(fox);
+            }
+        }
+
+        public void SimulateRound()
+        {
+            GrowGrass();
+        }
+        public string GetGrassType(int x, int y)
+        {
+            if (x >= 0 && x < width && y >= 0 && y < height)
+            {
+                return grassMatrix[x, y].GetTheTypeOfGrass();
+            }
+            else
+            {
+                return "Invalid Coordinates";
+            }
+        }
+
+
+        private void GrowGrass()
+        {
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    // Fű nő, ha nincs nyúl vagy róka a területen
+                    if (grassMatrix[i, j].NutritionalValue < 2)
+                    {
+                        grassMatrix[i, j] = new Grass(grassMatrix[i, j].NutritionalValue + 1);
+                    }
+                }
+            }
+        }
     }
 }
